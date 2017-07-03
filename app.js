c3fd4ae0973bb9b71c61e5e3d6d437a1425b9e80
Alec Schleicher <schleicher@3cisd.com>
2017-07-03 18:22:57 -0400

    added styling and most gameplay functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyA-swMEXjygwuzzOqvGBLzQVOuegR1s_5Q",
@@ -8,70 +10,208 @@ var config = {
     messagingSenderId: "17264456920"
 };
 firebase.initializeApp(config);
-
-// Reference Database
 const database = firebase.database();
 
+// Handle New Players
 var player = 0;
-
-// Player Join
 $("#submit").click(function() {
- 
-    var playerName = $("input").val();
 
-    // If No Active Player 1...
+    // Check for valid input and player spots
+    var playerName = $("input").val();
     database.ref("players").once('value', function(snapshot) {
-        if (snapshot.val()["1"] === undefined) {
+
+        // If No Active Player 1...
+        if (snapshot.val()["1"] === undefined && playerName.trim() !== "") {
+
+            // Clear Rules and Signup / Show Game
+            $("#rules").addClass("hidden");
+            $("#signup").addClass("hidden");
+            $(".playerbox").removeClass("hidden");
+            $("#statusText").removeClass("hidden");
+        
             // Write to DB
-            if (playerName !== "") {
-                database.ref("players/1").set({
-                    name: playerName.trim(),
-                    wins: 0,
-                    losses: 0
-                });
-                database.ref("players/1").onDisconnect().set({
-                    1: null
-                });
-            }
             player = 1;
-        } else if (snapshot.val()["2"] === undefined) {
+            database.ref("players/1").set({
+                name: playerName.trim(),
+                wins: 0,
+                losses: 0
+            });
+            database.ref("players/1").onDisconnect().set({
+                1: null
+            });
+
+        // Else If No Active Player 2...
+        } else if (snapshot.val()["2"] === undefined && playerName.trim() !== "") {
+        
+            // Clear Rules and Signup
+            $("#rules").addClass("hidden");
+            $("#signup").addClass("hidden");
+            $(".playerbox").removeClass("hidden");
+            $("#statusText").removeClass("hidden");
+            
             // Write to DB
-            if (playerName !== "") {
-                database.ref("players/2").set({
-                    name: playerName.trim(),
-                    wins: 0,
-                    losses: 0
-                });
-                database.ref("players/2").onDisconnect().set({
-                    2: null
-                });
-            }
             player = 2;
+            database.ref("players/2").set({
+                name: playerName.trim(),
+                wins: 0,
+                losses: 0
+            });
+            database.ref("players/2").onDisconnect().set({
+                2: null
+            });
         }
     })
 });
 
-// Update HTML on DB Value Change...
+// Update Player 1 HTML on DB Value Change...
 database.ref("players/1").on('value', function(snapshot) {
-    var name = snapshot.val().name;
-    var losses = snapshot.val().losses;
-    var wins = snapshot.val().wins;
-    $("#player1header").html(name);
-    if (player !== 0)
-        $("#player1").html("Hi " + name + "! You are Player 1!");
-    $("#player1footer").html("Wins: " + wins + " | Losses: " + losses);
+
+    if (snapshot.val() !== null) {
+        var name = snapshot.val().name;
+        var losses = snapshot.val().losses;
+        var wins = snapshot.val().wins;
+        $("#player1header").html(name);
+        if (player === 1) {
+            $("#player1").html("Hi " + name + "! You are Player 1!");
+        } else {
+            $("#player1").html("");
+        }
+        $("#player1footer").html("Wins: " + wins + " | Losses: " + losses);
+    } else {
+        var name = "Player 1";
+        var losses = "NA";
+        var wins = "NA";
+        $("#player1header").html(name);
+        $("#player1").html("Waiting on " + name + ".");
+        $("#player1footer").html("Wins: " + wins + " | Losses: " + losses);
+    }
 })  
+
+// Update Player 2 HTML on DB Value Change...
 database.ref("players/2").on('value', function(snapshot) {
-    var name = snapshot.val().name;
-    var losses = snapshot.val().losses;
-    var wins = snapshot.val().wins;
-    $("#player2header").html(name);
-    if (player !== 0)
-        $("#player2").html("Hi " + name + "! You are Player 2!");
-    $("#player2footer").html("Wins: " + wins + " | Losses: " + losses);
+
+    if (snapshot.val() !== null) {
+        var name = snapshot.val().name;
+        var losses = snapshot.val().losses;
+        var wins = snapshot.val().wins;
+        $("#player2header").html(name);
+        if (player === 2) {
+            $("#player2").html("Hi " + name + "! You are Player 2!");
+        } else {
+            $("#player2").html("");
+        }
+        $("#player2footer").html("Wins: " + wins + " | Losses: " + losses);
+    } else {
+        var name = "Player 2";
+        var losses = "NA";
+        var wins = "NA";
+        $("#player2header").html(name);
+        $("#player2").html("Waiting on " + name + ".");
+        $("#player2footer").html("Wins: " + wins + " | Losses: " + losses);
+    }
 })
 
-// Clear Player on DC
-if (player !== undefined) {
-    database.ref("players/"+player).onDisconnect().set("");
+// Check for Full Lobby (2 Players)
+database.ref("players").on('value', function(snapshot) {
+    
+    // If two players are in a game
+    if (snapshot.val()["1"] !== undefined && snapshot.val()["2"] !== undefined) {
+
+        // If both of them haven't made choices...
+        if (snapshot.val()["1"]["choice"] === undefined && snapshot.val()["2"]["choice"] === undefined) {
+            
+            // Make buttons and text
+            var text = $("<div>Choose an element!</div>");
+            var fire = $("<button id='fire'><span class='glyphicon glyphicon-fire'></span></button>");
+            fire.addClass("btn btn-danger");
+            var nature = $("<button id='nature'><span class='glyphicon glyphicon-leaf'></span></button>");
+            nature.addClass("btn btn-success");
+            var water = $("<button id='water'><span class='glyphicon glyphicon-tint'></span></button>");
+            water.addClass("btn btn-primary");
+            var buttons = $("<div>");
+            buttons.append(fire, nature, water);
+
+            var otherPlayer = JSON.stringify(Math.abs(player-3));
+
+            // Add click handlers
+            fire.click(function() {
+
+                // Update DB
+                database.ref("players/" + player).update({
+                    choice: $(this).attr("id")
+                })
+
+                // Update HTML
+                $("#player" + player).html(fire);
+                $("#statusText").html("Waiting on " + snapshot.val()[otherPlayer]["name"]);
+
+            })
+            nature.click(function() {
+
+                // Update DB
+                database.ref("players/" + player).update({
+                    choice: $(this).attr("id")
+                })
+                
+                // Update HTML
+                $("#player" + player).html(nature);
+                $("#statusText").html("Waiting on " + snapshot.val()[otherPlayer]["name"]);
+
+            })
+            water.click(function() {
+
+                // Update DB
+                database.ref("players/" + player).update({
+                    choice: $(this).attr("id")
+                })
+                
+                // Update HTML
+                $("#player" + player).html(water);
+                $("#statusText").html("Waiting on " + snapshot.val()[otherPlayer]["name"]);
+
+            })
+
+            // Append buttons
+            if (player === 1) {
+                $("#player1").html(buttons);
+            } else if (player === 2) {
+                $("#player2").html(buttons);
+            }
+        
+        // Else if both players have made choices...
+        } else if (snapshot.val()["1"]["choice"] !== undefined && snapshot.val()["2"]["choice"] !== undefined) {
+            $("#statusText").html(snapshot.val()[gameResult(snapshot.val()["1"]["choice"], snapshot.val()["2"]["choice"])]["name"] + " wins!");
+
+        } else {
+            console.log("There has been some lapse in my logic");
+        }
+    }
+})
+
+})
+
+function gameResult(p1Choice, p2Choice) {
+    if (p1Choice === p2Choice) {
+        return -1;
+    } else if (p1Choice === "fire") {
+        if (p2Choice === "nature") { 
+            return 1;
+        } else {
+            return 2;
+        }
+    } else if (p1Choice === "nature") {
+        if (p2Choice === "water") { 
+            return 1;
+        } else {
+            return 2;
+        }
+    } else {
+        if (p2Choice === "fire") { 
+            return 1;
+        } else {
+            return 2;
+        }
+    }
 }
+
+
